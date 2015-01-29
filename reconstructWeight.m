c0955457e25426c3dea23da4d46cd4cb4c3fed71
wjj <williamjjohnston@gmail.com>
2015-01-29 14:24:15 -0600

    updated visualization functions

--- reconstructWeight.m
+++ reconstructWeight.m
@@ -3,12 +3,11 @@ function [ wm, dwm ] = reconstructWeight( wv, m, n, t )
 %   Detailed explanation goes here
 wm = zeros(m, n*t);
 dwm = zeros(m, n*(t-1));
-insz = m*n;
+wmpre = reshape(wv, t, m, n);
 for i = 1:t
-    wm(:, (i-1)*n + 1:n*i) = reshape(wv(insz*(i-1) + 1:insz*i), m, n);
+    wm(:, (i-1)*n + 1:n*i) = wmpre(i, :, :);
     if i > 1
-       dwm(:, (i-2)*n + 1:n*(i-1)) = wm(:, (i-1)*n + 1:n*i) - ...
-           wm(:, (i-2)*n + 1:n*(i-1));
+        dwm(:, (i-2)*n + 1:n*(i-1)) = wmpre(i, :, :) - wmpre(i-1, :, :);
     end
 end
 end
