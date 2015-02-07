de08a9f9ca0b033774dccb130f58a4eb9e9615c2
wjj <williamjjohnston@gmail.com>
2015-02-06 18:07:09 -0600

    misc improvements to visualization function

--- visualizePrediction.m
+++ visualizePrediction.m
@@ -4,7 +4,7 @@ function [  ] = visualizePrediction( predOut, m, n, t )
 
 
 %% make histograms of error
-figure(1); hold on;
+figure(); hold on;
 
 subplot(2, 1, 1);
 seenSSError = sumSqErrorByTrial(predOut.seen.error);
@@ -17,20 +17,20 @@ unseenSSError = sumSqErrorByTrial(predOut.unseen.error);
 histogram(unseenSSError);
 title(sprintf('sum squared error for %i untrained trials; m=%f, s=%f', ... 
     length(unseenSSError), mean(unseenSSError), std(unseenSSError))); 
-
 hold off;
 
 %% make image of prediction vs truth
-figure(2); hold on;
+figure(); hold on;
 [merr, mindex] = min(seenSSError);
-subplot(1, 2, 1);
+subplot(1, 2, 1); 
 origIm = reshape(predOut.seen.orig(mindex, :), m, n);
 imagesc(origIm);
 colormap gray;
 title('original; seen');
 set(gca, 'XTickLabel', []);
 set(gca, 'YTickLabel', []);
-subplot(1, 2, 2);
+
+subplot(1, 2, 2); 
 predIm = reshape(predOut.seen.predict(mindex, :), m, n);
 imagesc(predIm);
 colormap gray;
@@ -39,26 +39,31 @@ set(gca, 'XTickLabel', []);
 set(gca, 'YTickLabel', []);
 hold off;
 
-figure(3); hold on;
+figure(); hold on;
 [merr, mindex] = min(unseenSSError);
-subplot(1, 2, 1);
+subplot(1, 2, 1); 
 origIm = reshape(predOut.unseen.orig(mindex, :), m, n);
 imagesc(origIm);
 colormap gray; 
+axis equal;
 title('original; unseen');
 set(gca, 'XTickLabel', []);
 set(gca, 'YTickLabel', []);
-subplot(1, 2, 2);
+
+subplot(1, 2, 2); 
 predIm = reshape(predOut.unseen.predict(mindex, :), m, n);
 imagesc(predIm);
 colormap gray;
+axis equal;
+pbaspect('manual');
 title(sprintf('predicted; unseen; error=%f', merr));
 set(gca, 'XTickLabel', []);
 set(gca, 'YTickLabel', []);
 hold off;
 
 %% make images of weights
-figure(4); hold on;
+figure(); hold on;
+title('weight reconstructions');
 weights = predOut.net.trainedNet.W{1};
 numweights = size(weights, 1);
 reconnedWeights = cell(numweights, 1);
@@ -75,7 +80,8 @@ for i = 1:numweights
    reconnedDWeights{i} = dwm;
 end
 hold off;
-figure(5); hold on;
+figure(); hold on;
+title('weight reconstructions, temporal');
 for i = 1:numweights
    subplot(plotdim, plotdim, i);
    imagesc(reconnedDWeights{i});
